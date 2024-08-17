21bf06311f5553eb1acfa1686c322bd89e225a27
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-18 02:56:22 +0500

    shifted the route array into another component to make the code look clean

---
+++ utils/RoutesArray.ts
@@ -0,0 +1,38 @@
+export const routes = [
+  {
+    id: 1,
+    label: 'Dashboard',
+    Icon: 'https://cdn.iconscout.com/icon/premium/png-256-thumb/dashboard-1888158-1599936.png?f=webp',
+    href: '/dashboard',
+  },
+  {
+    id: 2,
+    label: 'Conversation',
+    Icon: '360_F_500526719_3viiy0ih3f5d85kYlQJ8IsNwNzhUiPVK-removebg-preview.png',
+    href: '/Conversation',
+  },
+  {
+    id: 3,
+    label: 'Summary Generator',
+    Icon: 'https://static.thenounproject.com/png/645097-200.png',
+    href: '/SummaryGenerator',
+  },
+  {
+    id: 4,
+    label: 'HashTags Provider',
+    Icon: 'https://cdn-icons-png.flaticon.com/512/1827/1827975.png',
+    href: '/HashTagsProvider',
+  },
+  {
+    id: 5,
+    label: 'Music Choice Generator',
+    Icon: 'https://static.thenounproject.com/png/6708663-200.png',
+    href: '/MusicGeneration',
+  },
+  {
+    id: 6,
+    label: 'Code Generation',
+    Icon: 'https://icons.veryicon.com/png/o/miscellaneous/programming-software-icons/generate-code.png',
+    href: '/CodeGeneration',
+  },
+]
