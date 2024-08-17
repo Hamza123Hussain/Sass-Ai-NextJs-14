915f6e22d7b8bdf6514a611bfad748ef6a876ad8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-18 02:57:04 +0500

    sidebar hidden till screen is md

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,12 @@
 import React, { useState } from 'react'
 
-import { UserButton } from '@clerk/nextjs'
 import MobileSideBar from './MobileSidebar/MobileSideBar'
 
 const Navbar = () => {
   return (
-    <div className=" flex items-center p-2">
+    <div className=" flex items-center p-2 md:hidden">
       <MobileSideBar />
-      <div className=" flex w-full justify-end">
-        <UserButton afterSignOutUrl="/" />
-      </div>
+      <div className=" flex w-full justify-end"></div>
     </div>
   )
 }
