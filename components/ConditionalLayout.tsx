78fb86d8111178bca0bf2f70179a83be1019a757
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-20 04:57:32 +0500

    giving the complete screen

--- components/ConditionalLayout.tsx
+++ components/ConditionalLayout.tsx
@@ -31,7 +31,7 @@ const ConditionalLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       {userData ? (
-        <div className="flex h-screen">
+        <div className="flex min-h-screen">
           {/* Sidebar */}
           <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:top-0 md:left-0 md:h-full bg-slate-800 text-white">
             <Sidebar />
@@ -39,7 +39,7 @@ const ConditionalLayout = ({ children }: { children: ReactNode }) => {
 
           <div className="flex-1 flex flex-col md:ml-64">
             <Navbar />
-            <main className="flex-1 overflow-y-auto bg-gray-100 ">
+            <main className="flex-1 min-h-screen overflow-y-auto bg-gray-100 ">
               {children}
             </main>
           </div>
