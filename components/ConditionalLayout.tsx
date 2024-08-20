9bfd88564b5cc3c43332eda01ec290aeb0f6eef3
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-20 19:43:02 +0500

    turned off sidebar as of now

--- components/ConditionalLayout.tsx
+++ components/ConditionalLayout.tsx
@@ -31,18 +31,18 @@ const ConditionalLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       {userData ? (
-        <div className="flex min-h-screen">
-          {/* Sidebar */}
+        <div className="flex flex-col min-h-screen">
+          {/* Sidebar
           <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:top-0 md:left-0 md:h-full bg-slate-800 text-white">
             <Sidebar />
-          </div>
+          </div> */}
 
-          <div className="flex-1 flex flex-col md:ml-64">
-            <Navbar />
-            <main className="flex-1 min-h-screen overflow-y-auto bg-gray-100 ">
-              {children}
-            </main>
-          </div>
+          {/* <div className="flex-1 flex flex-col md:ml-64"> */}
+          <Navbar />
+          <main className="flex-1 min-h-screen overflow-y-auto bg-gray-100 ">
+            {children}
+          </main>
+          {/* </div> */}
         </div>
       ) : isAuthPage ? (
         <div className="flex flex-col h-screen">
