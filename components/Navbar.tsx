b2a57d2a77f1c3ba70a3fe6085a86ac35173e48c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-20 19:42:41 +0500

    made sheet available for all screens

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import MobileSideBar from './MobileSidebar/MobileSideBar'
 
 const Navbar = () => {
   return (
-    <div className=" flex items-center p-2 md:hidden">
+    <div className=" flex items-center p-2 bg-slate-900 border-b-2  border-slate-700 ">
       <MobileSideBar />
     </div>
   )
