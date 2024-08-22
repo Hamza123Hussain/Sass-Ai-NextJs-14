0e79c4613be7986942a5e65ffb40da8a042e8e36
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-22 19:39:28 +0500

    hOME PAGE MADE

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,37 @@
+import React from 'react'
 import { Button } from '@/components/ui/button'
 
-import React from 'react'
+import { LayoutDashboard, LucideIcon } from 'lucide-react'
+import { routes } from '@/utils/RoutesArray'
+import Link from 'next/link'
 
 const DashBoard = () => {
   return (
-    <div>
-      I am CONTNENT
-      <Button value={'hello'} variant={'destructive'} />
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-8 flex items-center gap-2">
+        Welcome to{' '}
+        <Link
+          href={'/dashboard'}
+          className=" flex  items-center justify-center  gap-2 "
+        >
+          <LayoutDashboard />
+
+          <h1 className=" font-bold text-2xl">Quick Suggest</h1>
+        </Link>{' '}
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+        {routes.map(({ id, label, Icon, href }) => (
+          <a
+            key={id}
+            href={href}
+            className="flex flex-col items-center p-4 bg-gray-600 rounded-lg shadow-lg transform transition hover:scale-105"
+          >
+            <img src={Icon} alt={label} className="h-12 w-12 mb-4" />
+            <span className="text-xl font-semibold">{label}</span>
+          </a>
+        ))}
+      </div>
     </div>
   )
 }
